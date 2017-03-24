93ec9388d616af39a2abe3df90642e81d67acff3
heriyanto <heriyanto@Changes-MacBook-Pro.local>
2017-03-24 14:49:50 +0700

    add complete files

---
+++ amivar.tf
@@ -0,0 +1,1 @@
+variable "AMI_ID" { default = "ami-c9239eaa" }
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_REGION" {
+  default = "ap-southeast-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
